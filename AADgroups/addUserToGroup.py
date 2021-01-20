74e480d60f649eae2f46b282055fb2485f31b134
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2021-01-21 09:11:55 +1300

    code to get user details added

--- AADgroups/addUserToGroup.py
+++ AADgroups/addUserToGroup.py
@@ -9,35 +9,60 @@ tenant_id = '35558aca-3637-44e9-8cc7-393f0482cb28'
 client_secret = '0_D_QnRos~h0wHmo0E1Z6Fwzy_JS.W67O3'
 
 # function to get graph api token
-def get_tokenOLD(client_id, tenant_id, client_secret):
-    scope = ["https://graph.microsoft.com/.default"]
-    app = msal.ConfidentialClientApplication(client_id,
-                        authority='https://login.microsoftonline.com/{}'.format(tenant_id),
-                        client_credential=client_secret
-                        )
-    result = app.acquire_token_silent(scopes=scope, account=None)
-    if not result:
-        result = app.acquire_token_for_client(scopes=scope)
-    return result['access_token']
+#def get_tokenOLD(client_id, tenant_id, client_secret):
+#    scope = ["https://graph.microsoft.com/.default"]
+#    app = msal.ConfidentialClientApplication(client_id,
+#                        authority='https://login.microsoftonline.com/{}'.format(tenant_id),
+#                        client_credential=client_secret
+#                        )
+#    result = app.acquire_token_silent(scopes=scope, account=None)
+#    if not result:
+#        result = app.acquire_token_for_client(scopes=scope)
+#    return result['access_token']
 
-# function to get a user
-def get_token(config)
+# function to get a auth token
+def get_token(client_id, authority, secret,scope):
+    print(client_id)
+    print(authority)
+    print(secret)
+    print(scope)
     app = msal.ConfidentialClientApplication(
-        config["client_id"], authority=["authority"],
-        client_credential=["secret"],
+        client_id, authority=authority,
+        client_credential=secret,
     )
     result = None
     
-    result = app.acquire_token_silent(config["scope"], account=None)
+    result = app.acquire_token_silent(scope, account=None)
 
     if not result:
-        result = app.acquire_token_for_client(scopes=config["scope"])
+        result = app.acquire_token_for_client(scopes=scope)
 
-    
+    return result['access_token']
+
+def getAADuser(token, endpoint):
+    graph_data = requests.get(  # Use token to call downstream service
+        endpoint + '/ed.haszard@ihc.org.nz',
+        headers={'Authorization': 'Bearer ' + token},).json()
+        
+    print("Graph API call result: %s" % json.dumps(graph_data, indent=2))
+
 # load parameters from JSON file in args
-config = json.load(open(sys.argv[1]))
+# config = json.load(open(sys.argv[1]))
+
+# load parameters from JSON file
+with open("AADgroups\msal-parametersEdTestIHC.json") as json_file:
+    config = json.load(json_file)
+
+client_id = config["client_id"]
+authority = config["authority"]
+secret = config["secret"]
+scope = config["scope"]
+endpoint = config["endpoint"]
 
 # get a Graph API token to use for API calls
-token = get_token(client_id, tenant_id, client_secret)
+token = get_token(client_id, authority, secret, scope)
 
-print(token)
+# get user(s)
+getAADuser(token, endpoint)
+
+#print(token)
